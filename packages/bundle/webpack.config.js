2a1a396b1f630f06c7dcea7a52c971953e7b4b61
William Wong <compulim@hotmail.com>
2019-09-13 03:39:34 -0700

    Clean up

--- packages/bundle/webpack.config.js
+++ packages/bundle/webpack.config.js
@@ -4,58 +4,12 @@ module.exports = {
   entry: {
     helloworld: './lib/index.js',
   },
-  // externals: {
-  //   react: {
-  //     commonjs: 'react',
-  //     commonjs2: 'react',
-  //     root: 'React'
-  //   },
-  //   'react-dom': {
-  //     commonjs: 'react-dom',
-  //     commonjs2: 'react-dom',
-  //     root: 'ReactDOM'
-  //   }
-  // },
   resolve: {
     alias: {
       react: resolve(__dirname, 'node_modules/isomorphic-react/dist/react.js'),
-      'react-dom': resolve(__dirname, 'node_modules/isomorphic-react-dom/dist/react-dom.js'),
-      // 'react-dom': resolve(__dirname, 'node_modules/isomorphic-react-dom/lib/ReactDOM.js'),
+      'react-dom': resolve(__dirname, 'node_modules/isomorphic-react-dom/dist/react-dom.js')
     }
-    // alias: {
-    //   react: resolve(__dirname, 'src/isomorphic/React.js'),
-    //   'react-dom': resolve(__dirname, 'src/isomorphic/ReactDOM.js')
-    // }
   },
-  // externals: {
-  //   react: 'node_modules/isomorphic-react/dist/react.js',
-  //   'react-dom': 'node_modules/isomorphic-react-dom/dist/react-dom.js'
-  // },
-  // externals: [
-  //   function (context, request, callback) {
-  //     console.log(`${ request }: ${ context }`);
-
-  //     if (/node_modules/u.test(context)) {
-  //       return callback();
-  //     } else if (/lib[\/\\]npmPackages/u.test(context)) {
-  //       if (request === 'react') {
-  //         // return callback(null, 'commonjs react');
-  //         return callback();
-  //       } else if (request === 'react-dom') {
-  //         // return callback(null, 'commonjs reactdom');
-  //         return callback();
-  //       }
-  //     } else if (/lib[\/\\]window/u.test(context)) {
-  //       if (request === 'react') {
-  //         return callback(null, 'root React');
-  //       } else if (request === 'react-dom') {
-  //         return callback(null, 'root ReactDOM');
-  //       }
-  //     }
-
-  //     callback();
-  //   }
-  // ],
   mode: 'development',
   // mode: 'production',
   output: {
